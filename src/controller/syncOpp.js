b5f18d578f7c5e2b3191bb806a502f0d5e8958bf
lixiao8 <13003135616@163.com>
2021-07-26 13:26:10 +0800

    auth

--- src/controller/syncOpp.js
+++ src/controller/syncOpp.js
@@ -158,7 +158,7 @@ const sync = async (data, repeat) => {
     options.url = cacheDta.cpihost;
     options.headers = { 
         "Content-Type": "application/json", 
-        'Authorization': 'Basic UzAwMTg1ODI1NTk6SXR0czEyMzRAOA=='
+        'Authorization': 'Basic UzAwMTg1ODI1NTk6SXR0czEyMzRAMDE='
         // "Connection":"keep-alive"
     };
     options.method = 'POST';
