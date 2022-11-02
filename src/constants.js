91c176a74e55c4bd890adc979ea87b62a22362a6
lixiao8 <13003135616@163.com>
2022-11-02 10:46:43 +0800

    new password

--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,5 @@ export const cacheDta = {
     host: 'https://twmi.crm.tecowestinghouse.ca:444/api/data/v8.1/',
     cpihost: 'https://l200033-iflmap.hcisbp.ap1.hana.ondemand.com/http/twmi/oppsync',
     cpiAcchost: 'https://l200033-iflmap.hcisbp.ap1.hana.ondemand.com/http/twmi/accsync',
-    authData: "Basic UzAwMTg1ODI1NTk6SXR0czEyMzRANA=="
+    authData: "Basic UzAwMTg1ODI1NTk6SXR0czEyMzRANQ=="
 }
