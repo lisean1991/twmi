f535829cfe16fd6b9848b79fbf9f0dc512aeab2b
lixiao8 <13003135616@163.com>
2024-04-12 16:12:27 +0800

    0412

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const cacheDta = {
     runFlagAcc: 0,
     TWMI_COOKIE: '',
     host: 'https://twmi.crm.tecowestinghouse.ca:444/api/data/v8.1/',
-    cpihost: 'https://l200033-iflmap.hcisbp.ap1.hana.ondemand.com/http/twmi/oppsync',
-    cpiAcchost: 'https://l200033-iflmap.hcisbp.ap1.hana.ondemand.com/http/twmi/accsync',
+    cpihost: 'https://integration-suite-aefs9v3d.it-cpi006-rt.cfapps.jp10.hana.ondemand.com/http/twmi/oppsync',
+    cpiAcchost: 'https://integration-suite-aefs9v3d.it-cpi006-rt.cfapps.jp10.hana.ondemand.com/http/twmi/accsync',
     authData: "Basic UzAwMTg1ODI1NTk6SXR0czEyMzRAMDAy"
 }
