758691043d9a5069b302709b1155242cd8db437b
lixiao8 <13003135616@163.com>
2024-07-04 17:06:39 +0800

    user

--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,5 @@ export const cacheDta = {
     host: 'https://twmi.crm.tecowestinghouse.ca:444/api/data/v8.1/',
     cpihost: 'https://integration-suite-aefs9v3d.it-cpi006-rt.cfapps.jp10.hana.ondemand.com/http/twmi/oppsync',
     cpiAcchost: 'https://integration-suite-aefs9v3d.it-cpi006-rt.cfapps.jp10.hana.ondemand.com/http/twmi/accsync',
-    authData: "Basic UzAwMTg1ODI1NTk6SXR0czEyMzRAMDAy"
+    authData: "Basic UzAwMTg1ODI1NTk6SXR0czEyMzRAMDU="
 }
